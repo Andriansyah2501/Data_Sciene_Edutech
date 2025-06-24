c13f263af70aff6cf5cf53f351796514ded0edef
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-06-24 14:28:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import joblib
-import pandas as pd
-import numpy as np
 
 # Set page config (di awal setelah import st)
 st.set_page_config(page_title='Dropout Probability Predictor', layout='wide')
